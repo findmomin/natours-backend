e01e9f17578a555eafb2c531d7ce535152729b64
Abdul Momin <abdulmomin114477@gmail.com>
2021-08-04 02:31:27 +0600

    all

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ hello: 'Hello' });
+// Parsing tours
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// Get tours
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
+});
+
+// Post tours
+app.post('/api/v1/tours', (req, res) => {
+  const id = tours[tours.length - 1].id + 1;
+  const newTour = {
+    id,
+    ...req.body,
+  };
+  tours.push(newTour);
+
+  // Updating the tour file
+  console.log(newTour);
 });
 
 app.listen(port, () => {
