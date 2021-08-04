2a16d557f14015b639c2b34a794dfaef28def746
Abdul Momin <abdulmomin114477@gmail.com>
2021-08-04 07:04:03 +0600

    all

---
+++ controllers/tourController.js
@@ -0,0 +1,128 @@
+const fs = require('fs');
+
+// Parsing tours
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+// Get tours
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
+};
+
+// Get single tour
+exports.getSingleTour = (req, res) => {
+  const { id } = req.params;
+  const tour = tours.find((existingTour) => existingTour.id === +id);
+
+  // If no tour found
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid id',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+};
+
+// Post a tour
+exports.postTour = (req, res) => {
+  const id = tours[tours.length - 1].id + 1;
+  const newTour = {
+    id,
+    ...req.body,
+  };
+  tours.push(newTour);
+
+  // Updating the tour file
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) return res.status(500).json({ status: 'fail' });
+
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+// Update a tour
+exports.updateTour = (req, res) => {
+  const { id } = req.params;
+  const tour = tours.find((existingTour) => existingTour.id === +id);
+
+  // If no tour found
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid id',
+    });
+
+  const updatedTour = {
+    ...tour,
+    ...req.body,
+  };
+
+  // Updating the tours
+  const updatedTours = tours.map((existingTour) =>
+    existingTour.id === updatedTour.id ? updatedTour : existingTour
+  );
+
+  // Updating the tour file
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(updatedTours),
+    (err) => {
+      if (err) return res.status(500).json({ status: 'fail' });
+
+      res.status(200).json({
+        status: 'success',
+        data: { tour: updatedTour },
+      });
+    }
+  );
+};
+
+// Delete a tour
+exports.deleteTour = (req, res) => {
+  const { id } = req.params;
+  const tour = tours.find((existingTour) => existingTour.id === +id);
+
+  // If no tour found
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid id',
+    });
+
+  // Updating the tours
+  const updatedTours = tours.filter(
+    (existingTour) => tour.id !== existingTour.id
+  );
+
+  // Updating the tour file
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(updatedTours),
+    (err) => {
+      if (err) return res.status(500).json({ status: 'fail' });
+
+      res.status(204).json({
+        status: 'success',
+        data: null,
+      });
+    }
+  );
+};
---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+// Get all users
+exports.getAllUsers = (req, res) => {
+  res.json({ status: 'Good' });
+};
+
+// Create an user
+exports.createUser = (req, res) => {
+  res.json({ status: 'Good' });
+};
+
+// Get single user
+exports.getSingleUser = (req, res) => {
+  res.json({ status: 'Good' });
+};
+
+// Update an user
+exports.updateUser = (req, res) => {
+  res.json({ status: 'Good' });
+};
+
+// Delete an user
+exports.deleteUser = (req, res) => {
+  res.json({ status: 'Good' });
+};
