0c205a67f742c9c34eea205c42fec8b6e99b0348
Abdul Momin <abdulmomin114477@gmail.com>
2021-08-12 15:24:43 +0600

    all

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,7 +24,26 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password.'],
     minlength: 8,
+    validate: {
+      // Only works on create or save
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Password and password confirm must match!',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // If the password is modified don't hash it
+  if (!this.isModified('password')) return next();
+
+  // Hash the password on creation and delete the passwordConfirm
+  // field
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
