e07b917edd350c39e5c0f393394227cbf8463005
Abdul Momin <abdulmomin114477@gmail.com>
2021-08-14 13:57:10 +0600

    Added authorization

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const { catchAsync } = require('../helpers');
+const AppError = require('../utils/appError');
 const User = require('../models/userModel');
 
 // Get all users
@@ -11,6 +12,30 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     results: users.length,
     data: { users },
   });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // Create error if user posts password data
+  if (req.body.password || req.body.passwordConfirm)
+    return next(new AppError('This route is not for password updates', 400));
+
+  // Update user doc
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    { name: req.body.name, email: req.body.email },
+    { new: true, runValidators: true }
+  );
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  //
 });
 
 // Create an user
